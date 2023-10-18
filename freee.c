dbe0c455d8d49358dd2f6e3a9c30977afc5eb074
madiocre <wolvirex@gmail.com>
2023-10-19 01:08:44 +0200

    Added_All_Commits

---
+++ freee.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * ffree - frees a string array
+ * @str: string array to be freed
+ *
+ * Return: void
+ */
+void ffree(char **str)
+{
+  int i = 0;
+  
+  while (str[i] != NULL)
+  {
+    free(str[i]);
+    i++;
+  }
+  free(str);
+}
+
+/**
+ * mult_strcat - concatenates multiple strings
+ * @str: string to be created
+ * @str1: string to be concatenated
+ * @str2: string to be concatenated
+ * @str3: string to be concatenated
+ * 
+ * Return: void
+ */
+void mult_strcat(char *str,char *str1, char *str2, char *str3)
+{
+  strcat(str, str1);
+  strcat(str, str2);
+  strcat(str, str3);
+}
+
+  
--- main.c
+++ main.c
@@ -3,7 +3,53 @@
 /**
  * main - the Main fucntion of shell
  */
+extern int errno;
+int main(int ac, char **av, char **env) {
+  size_t buffer_size = MAX_SIZE;
+  char *line = NULL, *str = malloc(sizeof(char) * MAX_WIDTH),
+       *err = malloc(sizeof(char) * MAX_WIDTH), **argv;
+  int status;
+  pid_t childpid;
 
-int main(void)
-{
+  (void)ac;
+  check_interact();
+  while (getline(&line, &buffer_size, stdin) != -1) {
+    remove_newline(line);
+    argv = tokenize(line);
+    strcpy(str, "");
+    if(check_env(argv[0], env))
+    {
+      ffree(argv);
+      check_interact();
+      continue;
+    }
+    if (!strcmp(argv[0], "exit")) 
+    {
+      ffree(argv);
+      break;
+    }
+    strcat(str, argv[0]);
+    childpid = fork();
+    if (childpid < 0)
+      perror("\n");
+    else if (childpid == 0)
+    {
+      execve(str, argv, env);
+      mult_strcat(err, av[0], ": 1:",argv[0]);
+      perror(err);
+      exit(1);
+    }
+    else
+    {
+      wait(&status);
+      check_interact();
+    }
+    free(line);
+    line = NULL;
+    ffree(argv);
+  }
+  free(line);
+  free(str);
+  free(err);
+  return (0);
 }
