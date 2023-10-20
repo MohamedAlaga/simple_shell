fc7e77cc9e76ccf8ff2220001dd225e70ecbb135
MohamedAlaga <malaga.142014@gmail.com>
2023-10-20 22:39:56 +0300

    betty final

--- main.c
+++ main.c
@@ -1,34 +1,41 @@
 #include "shell.h"
 /**
  * main - the Main fucntion of shell
+ * @ac: number of arguments
+ * @av: arguments
+ * @env: environment variables
+ * Return: 0 on success
  */
-int main(int ac, char **av, char **env) {
-  size_t buffer_size = MAX_SIZE;
-  char *line = NULL, *str, *err = malloc(sizeof(char) * MAX_WIDTH), **argv;
-  int status, errno;
-  pid_t childpid;
-  (void)ac;
+int main(int ac, char **av, char **env)
+{
+size_t buffer_size = MAX_SIZE;
+char *line = NULL, *str, *err = malloc(sizeof(char) * MAX_WIDTH), **argv;
+int status, errno;
+pid_t childpid;
+(void)ac;
 first:
-  check_interact();
-  while (getline(&line, &buffer_size, stdin) != -1) {
-    str = malloc(sizeof(char) * MAX_WIDTH);
-    remove_newline(line);
-    if (strlen(line) == 0)
-      goto first;
-    argv = tokenize(line);
-    if (!strcmp(argv[0], "exit")) {
-      free(argv);
-      free(str);
-      break;
-    }
-    mult_strcat(str, argv[0]);
-    execute(str, argv, env,av);
-    free(line);
-    line = NULL;
-    free(argv);
-    argv = NULL;
-  }
-  free(line);
-  free(err);
-  return 0;
+check_interact();
+while (getline(&line, &buffer_size, stdin) != -1)
+{
+str = malloc(sizeof(char) * MAX_WIDTH);
+remove_newline(line);
+if (strlen(line) == 0)
+goto first;
+argv = tokenize(line);
+if (!strcmp(argv[0], "exit"))
+{
+free(argv);
+free(str);
+break;
+}
+mult_strcat(str, argv[0]);
+execute(str, argv, env, av);
+free(line);
+line = NULL;
+free(argv);
+argv = NULL;
+}
+free(line);
+free(err);
+return 0;
 }
